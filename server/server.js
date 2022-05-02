d7ec25191a12a3c9a1a9863e79b917d7c48ad8f4
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-05-02 11:37:02 -0400

    setting up apollo in the server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  // create a new Apollo server and pass in our data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+  });
+
+  // start the Apollo Server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  // this will create a special '/graphql' endpoint for the 'Express.js' server
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
