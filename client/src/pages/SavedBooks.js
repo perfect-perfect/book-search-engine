f485908c417d9de529aa49ac0f5148160a2b951f
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-05-02 21:14:06 -0400

    some changes to SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries'
+import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
